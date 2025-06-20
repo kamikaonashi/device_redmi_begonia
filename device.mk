8559c1f53fb1cb4691d03d2ff8a4dc886c31fb91
kamikaonashi <thorschi93@gmx.at>
2025-06-20 19:18:17 +0200

    begonia: De-voltag-ify

--- device.mk
+++ device.mk
@@ -116,7 +116,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-voltage
+    $(DEVICE_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
--- lineage_begonia.mk
+++ lineage_begonia.mk
@@ -15,23 +15,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from begonia device
 $(call inherit-product, device/redmi/begonia/device.mk)
 
-# Inherit some common VoltageOS stuff
-$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit some extras stuff
-$(call inherit-product-if-exists, vendor/extras/extras.mk)
-$(call inherit-product-if-exists, hardware/dolby/dolby.mk)
 $(call inherit-product-if-exists, vendor/MiuiCameraLeica/config.mk)
-
-# Boot animation
-TARGET_BOOT_ANIMATION_RES := 1920
-
-# Official-ify
-VOLTAGE_BUILD_TYPE := UNOFFICIAL
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := begonia
-PRODUCT_NAME := voltage_begonia
+PRODUCT_NAME := lineage_begonia
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
